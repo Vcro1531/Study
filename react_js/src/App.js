f2aa8ab603f55eefec3b7f3e64961aae7cbc6f26
Vcro1531 <jiminkim0520@gmail.com>
2022-11-15 09:29:28 +0900

    ⚡️ :: useEffect study

---
+++ react_js/src/App.js
@@ -0,0 +1,15 @@
+import React, { useEffect, useState } from "react";
+import Timer from "./Component/Timer";
+
+function App() {
+  const [showTimer, setShowTimer] = useState(false);
+
+  return (
+    <div>
+      {showTimer && <Timer />}
+      <button onClick={() => setShowTimer(!showTimer)}>Toggle Timer</button>
+    </div>
+  );
+}
+
+export default App;
