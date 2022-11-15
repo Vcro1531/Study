6536e0a527bf64c7b8e998c662bd6602583a9267
Vcro1531 <jiminkim0520@gmail.com>
2022-11-15 19:55:12 +0900

    ⚡️ :: useRef

--- react_js/src/App.js
+++ react_js/src/App.js
@@ -1,13 +1,36 @@
-import React, { useEffect, useState } from "react";
-import Timer from "./Component/Timer";
+import React, { useRef, useState } from "react";
 
 function App() {
-  const [showTimer, setShowTimer] = useState(false);
+  const [renderer, setRenderer] = useState(0);
+  const countRef = useRef(0);
+  let countVar = 0;
+
+  const doRendering = () => {
+    setRenderer(renderer + 1);
+  };
+
+  const increaseRef = () => {
+    countRef.current = countRef.current + 1;
+    console.log("ref:", countRef.current); //렌더링 되도 초기화 안됨
+  };
+
+  const increaseVar = () => {
+    countVar = countVar + 1;
+    console.log("Var:", countVar); //렌더링 되면 값이 0으로 초기화됨
+  };
+
+  const printResult = () => {
+    console.log(`ref: ${countRef.current}, var: ${countVar}`);
+  };
 
   return (
     <div>
-      {showTimer && <Timer />}
-      <button onClick={() => setShowTimer(!showTimer)}>Toggle Timer</button>
+      <p>Ref: {countRef.current}</p>
+      <p>Var: {countVar}</p>
+      <button onClick={doRendering}>render</button>
+      <button onClick={increaseRef}>Ref UP</button>
+      <button onClick={increaseVar}>Var UP</button>
+      <button onClick={printResult}>Ref Var 값 print</button>
     </div>
   );
 }
